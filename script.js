9d4f6d5201f73ca23b5109d2ecaf9363f05cb6c3
Jade-Melo <jademelo@etec.sp.gov.br>
2025-09-26 15:24:15 -0300

    Finalizando projeto

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.querySelector('.busca').addEventListener('submit', async(event)=>{
     if(input !== ""){
         showWarning("Carregando...")
 
-        let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
-        ${encodeURI(input)}&units=metric&lang=pt_br&appid=b0724cd1b2de128424ddda8f57f3a966`);
+        let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}
+        &units=metric&lang=pt_br&appid=b0724cd1b2de128424ddda8f57f3a966`);
         let json = await results.json();
 
         if(json.cod == 200){
