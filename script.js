090ecb31198cbc7b5261c01b52a6ca9970c63ab5
Jade-Melo <jademelo@etec.sp.gov.br>
2025-10-02 15:17:40 -0300

    Projeto em andamento

--- script.js
+++ script.js
@@ -8,12 +8,19 @@ document.querySelector('.busca').addEventListener('submit', async(event)=>{
     if(input !== ""){
         showWarning("Carregando...")
 
-        let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}
-        &units=metric&lang=pt_br&appid=b0724cd1b2de128424ddda8f57f3a966`);
+        let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ encodeURI (input) }
+        &appid=b0724cd1b2de128424ddda8f57f3a966&units=metrics&lang=pt_br`);
         let json = await results.json();
 
         if(json.cod == 200){
-
+            showInfo({
+                name:json.name,
+                country:json.sys.country,
+                temp:json.main.temp,
+                tempIcon:json.weather[0].icon,
+                windSpedd:json.wind.speed,
+                windAngle:json.wind.deg
+            })
         }else{
             showWarning("Não encontramos essa localização...")
         }
@@ -25,4 +32,9 @@ document.querySelector('.busca').addEventListener('submit', async(event)=>{
 
 function showWarning(msg){
     document.querySelector('.aviso').innerHTML = msg
+    document.querySelector('.resultado').style.display = 'block'
+}
+
+function showInfo(json){
+    showWarning("")
 }
